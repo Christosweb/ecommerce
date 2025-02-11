121488457336c878c83c96016c89905eb208c193
Christosweb <ajiboyetimileyin0@gmail.com>
2025-02-11 23:45:24 +0100

    order table styled

--- resources/views/product/order.blade.php
+++ resources/views/product/order.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>order history</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
+  <h3 class="text-center p-5">Order History</h3>
     <div class="overflow-x-auto">
-  <table class="table table-zebra">
+  <table class="table table-zebra border border-solid" >
     <!-- head -->
-    <thead>
+    <thead class="bg-black text-white">
       <tr>
         <th>id</th>
         <th>order identification</th>
@@ -23,13 +25,13 @@
     <tbody>
       <!-- row 1 -->
       @foreach ( $orderHistory as $order )
-      <tr>
-      <th>{{$order->id}}</th>
-        <th>{{$order->order_identification}}</th>
-        <th>{{$order->price}}</th>
-        <th>{{$order->quantity}}</th>
-        <th>{{$order->total_amount}}</th>
-        <th>{{$order->status}}</th>
+      <tr class="">
+      <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->id}}</th>
+        <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->order_identification}}</th>
+        <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->price}}</th>
+        <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->quantity}}</th>
+        <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->total_amount}}</th>
+        <th class="text-white {{$order->status=="completed"?"bg-success":'bg-red-600' }}">{{$order->status}}</th>
       </tr>
        @endforeach
     </tbody>
